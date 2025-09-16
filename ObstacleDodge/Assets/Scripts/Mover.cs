0cdf7576ceb47b92d18e05eca53f8bdd7727f17a
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-17 00:36:23 +0400

    Add collision effect

--- ObstacleDodge/Assets/Scripts/Mover.cs
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -6,10 +6,17 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
+        PrintInstructions();
     }
 
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+
         // instead of handling x, y, z separately, we could (and should) work with a Vector3
         // Unity's API is built around vectors
 
@@ -22,5 +29,12 @@ public class Mover : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
         transform.Translate(movement * moveSpeed * Time.deltaTime);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Press W-A-S-D to move.");
+        Debug.Log("Don't bump into objects!");
     }
 }
---
+++ ObstacleDodge/Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,12 @@
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bump!");
+
+        GetComponent<MeshRenderer>().material.color = new Color(0.30f, 0f, 0f);
+    }
+}
