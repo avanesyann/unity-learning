d90e1d89ac77029c17f3d4a64cac286e464aa89e
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-22 22:25:29 +0400

    Cachin Meshrenderer and RigidBody

--- ObstacleDodge/Assets/Scripts/Dropper.cs
+++ ObstacleDodge/Assets/Scripts/Dropper.cs
@@ -3,11 +3,24 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] float timeToDrop = 3f;
+    // Caching is a technique of storing frequently used data or information in memory
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false;
+    }
     void Update()
     {
         if (Time.time >= timeToDrop)
         {
-            GetComponent<Rigidbody>().useGravity = true;
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
         }
     }
 }
