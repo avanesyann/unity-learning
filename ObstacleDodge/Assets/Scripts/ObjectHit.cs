c5f4e7ad0606eca5216e6e744c10b4a3e4fdd45f
Narek Avanesyan <avanesyan12345@gmail.com>
2025-09-22 23:00:50 +0400

    Keep track of hit objects using tags

--- ObstacleDodge/Assets/Scripts/ObjectHit.cs
+++ ObstacleDodge/Assets/Scripts/ObjectHit.cs
@@ -5,8 +5,10 @@ public class ObjectHit : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        // Debug.Log("Bump!");
-
-        GetComponent<MeshRenderer>().material.color = new Color(0.30f, 0f, 0f);
+        if (collision.gameObject.tag == "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(0.30f, 0f, 0f);
+            gameObject.tag = "Hit";
+        }
     }
 }
--- ObstacleDodge/Assets/Scripts/Scorer.cs
+++ ObstacleDodge/Assets/Scripts/Scorer.cs
@@ -5,6 +5,11 @@ public class Scorer : MonoBehaviour
     int bumps = 0;
     void OnCollisionEnter(Collision collision)
     {
+        if (collision.gameObject.tag == "Hit")
+        {
+            return;
+        }
+        
         bumps++;
         Debug.Log($"You've bumped into a thing {bumps} times.");
     }
