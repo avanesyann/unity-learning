19b270b45103fe61875f6b37ba9f8a48a386c82b
Narek <avanesyan12345@gmail.com>
2026-07-22 18:00:00 +0400

    Fly rocky to space.

--- ObstacleDodge/Assets/Scripts/Mover.cs
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    float moveX = 0f;
+    float moveY = 0.01f;
+    float moveZ = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(moveX, moveY, moveZ);
     }
 }
