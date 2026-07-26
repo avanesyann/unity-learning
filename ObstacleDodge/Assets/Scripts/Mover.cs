c75fbcecdfc10a715d01342990eb98dcdd1b9f35
Narek <avanesyan12345@gmail.com>
2026-07-26 22:26:08 +0400

    Introduce Time.deltaTime.

--- ObstacleDodge/Assets/Scripts/Mover.cs
+++ ObstacleDodge/Assets/Scripts/Mover.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float movementSpeed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,9 +13,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
+        float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
         float moveY = 0f;
-        float moveZ = Input.GetAxis("Vertical");
+        float moveZ = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
 
         transform.Translate(moveX, moveY, moveZ);
     }
