5e04b8061a19ae8c910691803826639d61982ee6
Narek <avanesyan12345@gmail.com>
2026-08-06 22:01:01 +0400

    Caching components.

--- ObstacleDodge/Assets/Scripts/Dropper.cs
+++ ObstacleDodge/Assets/Scripts/Dropper.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
     [SerializeField] public float dropTime = 2f;
+
+    MeshRenderer dropperMesh;
+    Rigidbody dropperRb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        dropperMesh = GetComponent<MeshRenderer>();
+        dropperRb = GetComponent<Rigidbody>();
+
+        dropperMesh.enabled = false;
+        dropperRb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -14,7 +22,8 @@ public class Dropper : MonoBehaviour
     {
         if (Time.time > dropTime)
         {
-            GetComponent<Rigidbody>().useGravity = true;
+            dropperRb.useGravity = true;
+            dropperMesh.enabled = true;
         }
     }
 }
