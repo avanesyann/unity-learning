c08c51be4b49fd0bc4bdc307ec91f8547bd56072
Narek <avanesyan12345@gmail.com>
2026-08-20 22:31:30 +0400

    Fix obstacle not triggering.

--- ObstacleDodge/Assets/Scripts/TriggerProjectile.cs
+++ ObstacleDodge/Assets/Scripts/TriggerProjectile.cs
@@ -3,13 +3,11 @@ using UnityEngine;
 public class TriggerProjectile : MonoBehaviour
 {
     [SerializeField] GameObject spinner;
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        Debug.Log("bububu");
         if (other.gameObject.tag == "Player")
         {
             spinner.SetActive(true);
-            Debug.Log("Bup");
         }
     }
 }
